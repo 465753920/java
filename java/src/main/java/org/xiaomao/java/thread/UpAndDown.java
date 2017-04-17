667892f6e6421e8d53738cc1763e11a0fb2ccee2
ShenQiuS <465753920@qq.com>
2017-04-17 22:36:58 +0800

    add thread test

--- java/src/main/java/org/xiaomao/java/thread/UpAndDown.java
+++ java/src/main/java/org/xiaomao/java/thread/UpAndDown.java
@@ -2,9 +2,40 @@ package org.xiaomao.java.thread;
 
 public class UpAndDown {
 
+	private int balance = 0;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		UpAndDown uad = new UpAndDown();
+		uad.goThroughLife();
+		System.out.println(Thread.currentThread().getName());
+	}
 
+	public void goThroughLife() {
+
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				add();
+			}
+		}, "MyThread-1");
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				add();
+			}
+		}, "MyThread-2");
+
+		t1.start();
+		t2.start();
+	}
+
+	public void add() {
+		this.balance++;
+	}
+
+	public void subtract() {
+		this.balance--;
 	}
 
 }
