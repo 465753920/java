4ba13c1058d4f263a95d618de405b0a2863ff807
ShenQiuS <465753920@qq.com>
2017-10-17 14:01:35 +0800

    pom文件增加编译配置

---
+++ java/src/main/java/org/xiaomao/java/collection/map/Main.java
@@ -0,0 +1,18 @@
+package org.xiaomao.java.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("key", null);
+
+		System.out.println("Finish!");
+
+	}
+
+}
