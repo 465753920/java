9bdb6a602846d83445065f839f986a2c8c2df22a
ShenQiuS <465753920@qq.com>
2018-04-25 18:03:10 +0800

    增加MainClass类

---
+++ java/src/main/java/org/xiaomao/java/MainClass.java
@@ -0,0 +1,10 @@
+package org.xiaomao.java;
+
+import java.math.BigDecimal;
+
+public class MainClass {
+	public static void main(String[] args) {
+		BigDecimal bigDecimal = new BigDecimal(1163.0/100);
+		System.out.println(bigDecimal);
+	}
+}
